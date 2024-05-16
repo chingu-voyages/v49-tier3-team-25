03327af29b8d64d9393643327a4a522cac0b3dad
Atsutoshi Honda <haniyasusin@gmail.com>
2024-05-16 23:45:16 +0900

    feat: set up signin page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     </Router>
   );
