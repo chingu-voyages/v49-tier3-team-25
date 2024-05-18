7619c007b638231620a996c3afaa0dca24915941
Dicky Muhamad R <rizkyd@garena.co.id>
2024-05-18 13:23:08 +0000

    ignore and untrack server/build directory

--- server/build/middleware/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isAuthenticated = void 0;
-const isAuthenticated = (req, res, next) => {
-    // Check user authentication
-    next();
-};
-exports.isAuthenticated = isAuthenticated;
