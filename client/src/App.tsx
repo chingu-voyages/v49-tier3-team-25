1b52dee9136f9ce200c51b4afbc56651bf489902
jess <120062852+jessabc@users.noreply.github.com>
2024-05-22 14:24:02 +1000

    add layout & outlet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Route>
       </Routes>
     </Router>
   );
