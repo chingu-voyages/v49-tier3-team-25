ecac1bb134c74c86e70727a76dde9705e4d91475
Dicky Muhamad R <dickymr.dev@gmail.com>
2024-05-22 17:48:28 +0000

    feat: update validate middleware to support validation for body, params, and query

--- server/validations/user.ts
+++ server/validations/user.ts
@@ -2,12 +2,16 @@ import { z } from 'zod';
 
 export const userValidation = {
   signUp: z.object({
-    fullName: z.string(),
-    email: z.string().email(),
-    password: z.string().min(8),
+    body: z.object({
+      fullName: z.string(),
+      email: z.string().email(),
+      password: z.string().min(8),
+    }) 
   }),
   login: z.object({
-    email: z.string(),
-    password: z.string(),
+    body: z.object({
+      email: z.string(),
+      password: z.string(),
+    }) 
   }),
 };
