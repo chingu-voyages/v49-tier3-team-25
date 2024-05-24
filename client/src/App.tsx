72bab603e109db475f235a5ccee5294d88f23f16
jess <120062852+jessabc@users.noreply.github.com>
2024-05-24 12:44:26 +1000

    contact

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
