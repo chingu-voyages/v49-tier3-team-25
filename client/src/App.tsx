61f0e07a61f4feb9ceef96a58609fdcb2ea3cdb5
jess <120062852+jessabc@users.noreply.github.com>
2024-05-26 11:36:28 +1000

    order details

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
+import OrderDetail from "./pages/OrderDetail";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/my-orders/:id" element={<OrderDetail />} />
       </Routes>
     </Router>
   );
