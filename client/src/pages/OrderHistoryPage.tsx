349fc4d671b7db70cc82c1e8820e4a524429694f
jess <120062852+jessabc@users.noreply.github.com>
2024-05-26 17:02:00 +1000

    order history

---
+++ client/src/pages/OrderHistoryPage.tsx
@@ -0,0 +1,13 @@
+import OrderHistory from "../components/user-acccount/OrderHistory";
+
+export default function OrderHistoryPage() {
+  return (
+    <>
+      {/* <UserAccountLayout> */}
+      <div>
+        <OrderHistory />
+      </div>
+      {/* </UserAccountLayout> */}
+    </>
+  );
+}
