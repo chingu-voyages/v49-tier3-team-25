e54b868f1314eb143e768dde2559979c7fef630b
jess <120062852+jessabc@users.noreply.github.com>
2024-05-26 11:49:37 +1000

    move order detail to diff folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
-import OrderDetail from "./pages/OrderDetail";
+import OrderDetails from "./pages/OrderDetails";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/my-orders/:id" element={<OrderDetail />} />
+        <Route path="/my-orders/:id" element={<OrderDetails />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/OrderDetails.tsx
@@ -0,0 +1,13 @@
+import OrderDetail from "../components/user-account/OrderDetail";
+
+export default function OrderDetails() {
+  return (
+    <>
+      {/* <UserAccountLayout> */}
+      <div>
+        <OrderDetail />
+      </div>
+      {/* </UserAccountLayout> */}
+    </>
+  );
+}
