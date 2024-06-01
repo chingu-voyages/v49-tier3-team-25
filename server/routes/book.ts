acdd027fd72844b6a157ec8f0cab323a8873f51b
Dicky Muhamad R <dickymr.dev@gmail.com>
2024-06-01 17:38:13 +0700

    feat: add auth middleware

--- server/routes/book.ts
+++ server/routes/book.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { createBook, getBooks, getBookById, deleteBook, updateBook } from "../controllers/book";
 
+import { isAuthenticated } from "../middleware/isAuthenticated";
+
 const router = Router();
 
-router.post('/', createBook);
+router.post('/', isAuthenticated('admin'), createBook);
 router.get('/', getBooks);
 router.get('/:bookId', getBookById);
-router.delete('/:bookId', deleteBook);
-router.put('/:bookId', updateBook);
+router.delete('/:bookId', isAuthenticated('admin'), deleteBook);
+router.put('/:bookId', isAuthenticated('admin'), updateBook);
 
 export { router };
