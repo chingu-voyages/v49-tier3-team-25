e06dac5eb21b6b4a3a904b6c7ed5a22d6cce9d6d
jess <120062852+jessabc@users.noreply.github.com>
2024-06-13 16:33:46 +1000

    fix

--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -9,46 +9,61 @@ import { useLocation } from "react-router-dom";
 import "preline/preline";
 import { IStaticMethods } from "preline/preline";
 import { ToastContainer } from "react-toastify";
+import { setAdminCredentials } from "../../redux/features/adminAuth/adminAuthSlice";
 declare global {
   interface Window {
     HSStaticMethods: IStaticMethods;
   }
 }
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { RootState } from "../../redux/store";
 
 const Admin = () => {
+  const user = useAppSelector((state: RootState) => state.adminAuth.value);
   const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    window.HSStaticMethods.autoInit();
-  }, [location.pathname]);
-  console.log(location.pathname);
+    // check if user token in local storage
+    const user = JSON.parse(localStorage.getItem("adminUser") || '""');
+    // console.log(user);
+    if (user) {
+      dispatch(setAdminCredentials(user));
+    } else if (!user) {
+      navigate("/admin/signin");
+    }
+  }, []);
 
   return (
-    <div className="bg-stone-100">
-      {!location.pathname.includes("sign") && (
-        <>
-          <Header />
+    <>
+      <div className="bg-stone-100">
+        {!location.pathname.includes("sign") && (
+          <>
+            <Header />
 
-          <Sidebar />
-        </>
-      )}
+            <Sidebar />
+          </>
+        )}
 
-      <div className="w-full lg:ps-64">
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-        <Outlet />
+        <div className="w-full lg:ps-64">
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
